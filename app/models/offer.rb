0f143f5e04b944a754030208f16948808a739328
Shamil <badalloffsh@gmail.com>
2017-12-21 22:17:09 +0100

    Add: Projects Tasks Offers

---
+++ app/models/offer.rb
@@ -0,0 +1,9 @@
+class Offer < ApplicationRecord
+  belongs_to :project
+  has_many :tasks
+  has_and_belongs_to_many :contacts
+
+  enum status: [:active, :accepted, :rejected, :expired]
+
+  validates :name, presence: true
+end
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,4 +1,5 @@
 class ContactSerializer < ActiveModel::Serializer
-  belongs_to :company
   attributes :id, :family_name, :given_name, :company_id, :title, :phone, :email, :website, :address, :customer_id, :additional_info
+  belongs_to :company
+  has_many :projects
 end
