20746dfb25dc1c37838a5b404b6ffb667af5f6ce
Shamil <badalloffsh@gmail.com>
2017-12-23 00:55:46 +0100

    Add: customer interaction tracker (notes)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   # It looks like F&#9ING Mass assignment vulnerability MAN!!! WTF?!
   def relationship_params
     associations = {}
-    params[:data][:relationships].each do |key,value|
+    params.dig(:data, :relationships).each do |key, value|
       associations[key.to_sym] = if value[:data].is_a? Array
           value[:data].map { |data| find_related_object(data) }
       else
@@ -15,6 +15,8 @@ class ApplicationController < ActionController::API
   private
 
   def find_related_object(data)
-    data[:type].singularize.titlecase.constantize.find(data[:id])
+    return unless data[:type] && data[:id]
+    klass = data[:type].singularize.underscore.classify.safe_constantize
+    klass&.find(data[:id]) # klass.find(data[:id]) if klass
   end
 end
