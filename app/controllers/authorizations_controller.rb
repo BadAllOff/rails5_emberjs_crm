12eb78a79cfcd62e700f261c831acd67f939a77f
Shamil <badalloffsh@gmail.com>
2017-12-23 15:03:56 +0100

    Refactor: Authorization controller

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -1,10 +1,6 @@
 class AuthorizationsController < ActionController::API
   def create
-    auth_clent = Google::APIClient::ClientSecrets.new('web' => {
-        client_id: ENV['EMBER_APP_GOOGLE_CLIENT_ID'],
-        client_secret: ENV['EMBER_APP_GOOGLE_CLIENT_SECRET'],
-        redirect_url: 'http://localhost:4200/oauth2callback'
-    }).to_authorizaion
+    auth_clent = new_auth_client
     client.grant_type = 'authorization_code'
     client.code = params['authorization_code']
     token = client.fetch_access_token!
@@ -22,5 +18,15 @@ class AuthorizationsController < ActionController::API
       email: user_info.email,
       token: token
     }
+  end
+
+  private
+
+  def new_auth_client
+    Google::APIClient::ClientSecrets.new('web' => {
+        client_id: ENV['EMBER_APP_GOOGLE_CLIENT_ID'],
+        client_secret: ENV['EMBER_APP_GOOGLE_CLIENT_SECRET'],
+        redirect_url: 'http://localhost:4200/oauth2callback'
+    }).to_authorizaion
   end
 end
